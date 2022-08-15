70e11f7dfb44f0736cdea7a00e4d807d99d93e37
Rane Jasia <ranedavis@ranepours.attlocal.net>
2022-08-15 18:06:07 -0400

    part one flask done

--- forms.py
+++ forms.py
@@ -1,3 +1,4 @@
+from tokenize import String
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length
@@ -12,6 +13,15 @@ class AddUserForm(FlaskForm):
     password=PasswordField("Password",validators=[Length(min=6)])
     email=StringField("E-mail", validators=[DataRequired(),Email()])
     image_url=StringField('(Optional) Image URL')
+
+class UserEditForm(FlaskForm):
+    """form to edit users"""
+    username=StringField("Username",validators=[DataRequired()])
+    password=PasswordField("Password",validators=[Length(min=6)])
+    email=StringField("E-mail", validators=[DataRequired(),Email()])
+    image_url=StringField('(Optional) Image URL')
+    bio = StringField('(Optional) What do you want us to know?')
+    header_image_url = StringField('(Optional) Header Image URL')
 
 class LoginForm(FlaskForm):
     """Login form"""
